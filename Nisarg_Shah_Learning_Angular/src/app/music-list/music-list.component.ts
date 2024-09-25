27b26e3a82afa050a41b1324283b301475d77afb
Nisarg0845784 <W0845784@myscc.ca>
2024-09-25 18:34:20 -0400

    Generated 2 Component cards
    MusicList
    Songlist

---
+++ Nisarg_Shah_Learning_Angular/src/app/music-list/music-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './music-list.component.html',
+  styleUrl: './music-list.component.scss'
+})
+export class MusicListComponent {
+
+}
