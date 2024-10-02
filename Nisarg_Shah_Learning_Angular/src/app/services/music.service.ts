4a542e58151e30841be1ceb3309c12e5cee443d4
Nisarg0845784 <W0845784@myscc.ca>
2024-10-02 19:51:55 -0400

    Created CRUD methods

--- Nisarg_Shah_Learning_Angular/src/app/services/music.service.ts
+++ Nisarg_Shah_Learning_Angular/src/app/services/music.service.ts
@@ -6,10 +6,33 @@ import { musicList } from '../data/mockMusic';
   providedIn: 'root'
 })
 export class MusicService {
-
+  private music : Music[] = musicList;
   constructor() { }
   getMusic(): Observable<Music[]>{
     return of(musicList);
   }
 
-}
+  addMusic(newMusic:Music) : Observable<Music[]>{
+    this.music.push(newMusic)
+    return of(this.music);
+  }
+
+  updateMusic(updatedMusic:Music):Observable<Music[]>{
+    const index = this.music.findIndex(music => music.musicName === updatedMusic.musicName);
+    if (index !== -1) {
+      this.music[index] = updatedMusic;
+    }
+    return of(this.music);
+  }
+    deleteMusic(musicName:String):Observable<Music[]>{
+      this.music = this.music.filter(music => music.musicName != musicName);
+      return of(this.music)
+    }
+
+    getMusicByName(musicName:String):Observable<Music | undefined>{
+      const music = this.music.find(music =>music.musicName === musicName);
+      return of(music);
+    }
+  }
+
+
