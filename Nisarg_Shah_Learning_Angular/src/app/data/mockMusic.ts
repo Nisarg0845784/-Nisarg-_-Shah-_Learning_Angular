3a7d2b7e8a84bba471a03eb752d36315433736af
Nisarg0845784 <W0845784@myscc.ca>
2024-10-02 18:58:00 -0400

    Created new service of music

---
+++ Nisarg_Shah_Learning_Angular/src/app/data/mockMusic.ts
@@ -0,0 +1,11 @@
+import { Music } from "../models/music";
+
+export const musicList:Music[] =[ 
+     {musictype:"hiphop", musicName:"Millionare", musicDirector:"Yo Yo honey singh",musicViews: 100000, istraditional:false},
+    {musictype:"hiphop", musicName:"Paris Ka trip", musicDirector:"Yo Yo honey singh",musicViews: 200000, istraditional:false},
+   {musictype:"love", musicName:"Ae dil ae muskil", musicDirector:"Arjit Singh",musicViews: 300000, istraditional:false},
+    {musictype:"sad", musicName:"Phir bhi tumko Chaahunga", musicDirector:"Arjit Singh",musicViews: 400000, istraditional:false},
+   {musictype:"rock", musicName:"Rafta Rafta", musicDirector:"Suraj Jagan",musicViews: 10000, istraditional:false},
+   {musictype:"classical", musicName:"Saiyaan", musicDirector:"Arshad ali khan",musicViews: 500000, istraditional:true}
+];
+ 
