fc8b836ad31ff95a37111a6ace7d2291c9508dc4
Nisarg Shah <w0845784@myscc.ca>
2024-10-17 17:48:51 -0400

    Defining the list of routes

--- Nisarg_Shah_Learning_Angular/src/main.ts
+++ Nisarg_Shah_Learning_Angular/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Route, Routes } from '@angular/router';
+import { musicList } from './app/data/mockMusic';
+import { MusicListComponent } from './app/music-list/music-list.component';
+import { MusicListItemComponent } from './app/music-list-item/music-list-item.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path: '', redirectTo : '/music', pathMatch:'full'},
+  {path: 'music', component: MusicListComponent},
+  {path: 'music', component: MusicListItemComponent}
+];
