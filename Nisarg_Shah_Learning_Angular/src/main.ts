b41b515d8e80742f1f9adc8faa313e1cbd1c26c1
Nisarg Shah <w0845784@myscc.ca>
2024-11-14 18:16:26 -0500

    Applied lazy loading to all routes

--- Nisarg_Shah_Learning_Angular/src/main.ts
+++ Nisarg_Shah_Learning_Angular/src/main.ts
@@ -14,10 +14,18 @@ import { provideRouter } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo : '/music', pathMatch:'full'},
-  {path: 'music', component: MusicListComponent},
-  {path: 'music/:name', component: MusicListItemComponent},
-  {path: 'modify-list-item', component: ModifyListItemComponent},
-  {path: '**', component: PageNotFoundComponent}
+  {path: 'music',
+    loadComponent: ()=>
+      import('./app/music-list/music-list.component').then(m=>m.MusicListComponent)},
+  {path: 'music/:name', 
+    loadComponent : ()=> 
+      import('./app/music-list-item/music-list-item.component').then(m=>m.MusicListItemComponent)},
+  {path: 'modify-list-item', 
+    loadComponent : ()=>
+      import('./app/modify-list-item/modify-list-item.component').then(m=>m.ModifyListItemComponent)},
+  {path: '**', 
+    loadComponent: ()=>
+      import('./app/page-not-found/page-not-found.component').then(m=>m.PageNotFoundComponent)}
 ];
 bootstrapApplication(AppComponent,{
   providers: [provideRouter(routes)]
